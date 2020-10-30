08e2dd5903e042ea0f1f1a425db6ccd82657a4df
GermanShtey <gvshteynardt@gmail.com>
2020-10-30 16:03:03 +0300

    refactor: убрать console.log

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -16,7 +16,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
     async validate(email: string, password: string, full_name: string): Promise<any> {
         const user = await this.authService.validateUser(email, password, full_name);
-        console.log(user)
         if (!user) {
             throw new UnauthorizedException();
         }
